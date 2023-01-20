f192fc60424d917e9bae365a8c55f85dee0cc8da
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2023-01-20 16:23:19 -0500

    finished....

---
+++ app.js
@@ -0,0 +1,23 @@
+// Create a class for a person that has properties for the person's name, age, and address.
+var person = /** @class */ (function () {
+    function person(name, age, address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+    // The class should have a method that takes a string as an argument and returns the person's name and address concatenated together.
+    person.prototype.concatenated = function (seperatethis) {
+        {
+            if (typeof seperatethis !== "string") {
+                throw new Error("Separator must be a string");
+            }
+            ;
+            return this.name + seperatethis + this.address;
+        }
+    };
+    return person;
+}());
+var person1 = new person("Seth", 21, "Japan");
+console.log(person1);
+console.log(person1.concatenated(", "));
+// Use type annotations for the properties and method, and make sure to catch any errors that may occur when calling the method.
---
+++ app.ts
@@ -0,0 +1,47 @@
+// Create a class for a person that has properties for the person's name, age, and address.
+
+class person{
+
+    private name: string
+    private age: number
+    private address: string
+
+    constructor(name:string, age:number, address:string){
+        this.name = name
+        this.age = age
+        this.address = address
+    }
+    // The class should have a method that takes a string as an argument and returns the person's name and address concatenated together.
+
+    concatenated(seperatethis:string):string{
+    // Use type annotations for the properties and method, and make sure to catch any errors that may occur when calling the method. 
+        {if (typeof seperatethis !== "string"){
+        throw new Error("Separator must be a string")};
+
+        return this.name + seperatethis +this.address
+        }
+
+
+
+    }
+
+}
+const  person1 = new person("Seth", 21, "Japan")
+console.log(person1)
+console.log(person1.concatenated(", "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
